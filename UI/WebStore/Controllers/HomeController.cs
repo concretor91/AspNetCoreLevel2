30633de6895fec4f841bd038de277073d7ae75ff
Павел <pavel.zhilnikov@outlook.com>
2020-06-28 14:13:40 +1000

    Клиент для тестовго контроллера

--- UI/WebStore/Controllers/HomeController.cs
+++ UI/WebStore/Controllers/HomeController.cs
@@ -3,12 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebStore.Interfaces;
 
 namespace WebStore.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index() => View();
+        private readonly IValuesService valuesService;
+
+        public HomeController(IValuesService valuesService)
+        {
+            this.valuesService = valuesService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var values = await valuesService.GetAsync();
+            return View(values);
+        }
 
         public IActionResult Throw(string id) => throw new ApplicationException(id ?? "Error!");
 
